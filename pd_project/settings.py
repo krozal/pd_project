5a925d641eec5da2f4b16da9cbc8cb445c6d83ff
krozal <kamilroza95@gmail.com>
2023-06-24 18:16:41 +0200

    security updates

--- pd_project/settings.py
+++ pd_project/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kx39xt(q+mr=b0*&099bnm+ga&2h&vdkj=fe@w$z!t512gzs#o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
